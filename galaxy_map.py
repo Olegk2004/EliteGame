86d9c3410e83a4538fb8f361420ab1dceae97fe8
Olegk2004 <olegk_wot@mail.ru>
2024-03-13 15:38:40 +0300

    Реализовал визуализацию связей между планетами

--- galaxy_map.py
+++ galaxy_map.py
@@ -1,8 +1,18 @@
 import pygame
 import galaxy
+import settings
 
 
 def draw(galaxy, surface):
     # sky_surface = pygame.display.get_surface()
     for system in galaxy.systems:
-        pygame.draw.circle(surface, [0, 255, 0], (system.x, system.y), 2)
+        x = system.x / 260 * settings.SCREEN_WIDTH + 5
+        y = system.y / 260 * settings.SCREEN_HEIGHT + 5
+        pygame.draw.circle(surface, [0, 255, 0], (x, y), 4)
+    for match in galaxy.matches:
+        curr_x = match.x / 260 * settings.SCREEN_WIDTH + 5
+        curr_y = match.y / 260 * settings.SCREEN_HEIGHT + 5
+        for i in galaxy.matches[match]:
+            match_x = i.x / 260 * settings.SCREEN_WIDTH + 5
+            match_y = i.y / 260 * settings.SCREEN_HEIGHT + 5
+            pygame.draw.line(surface, settings.EDGES_COLOR, (curr_x, curr_y), (match_x, match_y), 1)
--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from player import Player
 from galaxy_plot import create_plot
 import galaxy_map
 
+
 class Game:
     def __init__(self):
         pygame.init()
@@ -34,20 +35,23 @@ class Game:
                     sys.exit()
             # Функция для запуска очередного "цикла" игрового процесса, я хз пока куда это положить, поэтому будет здесь
             print(f"Вы находитесь на планете:{player.current_planet.name}")
-            print(f"На вас напали космические пираты! У вас отняли {player.current_planet.pirates_value} литра топлива.")
+            print(
+                f"На вас напали космические пираты! У вас отняли {player.current_planet.pirates_value} литра топлива.")
             print(
                 f"Вы обнаружили заправочную станцию! Вы заправились на {player.current_planet.fuel_station_value} литра топлива.")
             player.fuel -= player.current_planet.pirates_value  # уменьшаем количество топлива на то, которое у нас отобрали пираты
             player.fuel += player.current_planet.fuel_station_value  # увеличиваем количество топлива на то, что мы нашли на станции
             print(f"У вас {player.fuel} литров топлива")
             print(f"вы можете прыгнуть на планеты:")
-            available = player.galaxy.matches[player.current_planet]  # список планет, на которые мы можем допрыгнуть с текущей
+            available = player.galaxy.matches[
+                player.current_planet]  # список планет, на которые мы можем допрыгнуть с текущей
             available_planets = {}  # словарь чтобы пользователь мог вводить планету на которую хочет прыгнуть с помощью цифры
             for i in range(
                     len(available)):  # проходимся по номерам планет, на которые мы можем прыгнуть и добавляем под ключом
                 # номера имя планеты
                 available_planets[str(i)] = available[i]
-                print(f"{i}. {available[i].name}, необходимо иметь топлива: {player.current_planet.distance_to(available[i])}")
+                print(
+                    f"{i}. {available[i].name}, необходимо иметь топлива: {player.current_planet.distance_to(available[i])}")
             planet = input("Введите номер планеты на которую хотите прыгнуть: ")
             is_true = player.jump(available_planets[planet])
             clear_output(wait=False)  # пока что не работает
