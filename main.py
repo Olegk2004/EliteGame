5b4e6650de3ef65a65de2221a2c0b0d1002a1a35
berberberk <pedchenkoti@gmail.com>
2024-03-13 12:45:49 +0300

    Вынес метанастройки (пока что тут только разрешение экрана) в отдельный модуль settings, содержащий константы для ширины и высоты окна

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 ''' Заготовка для игры
 import pygame
+from settings import *
 
 pygame.init()
-screen = pygame.display.set_mode((1000, 600))
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Super Elite Game")
 
 running = True
