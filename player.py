fdddf305a1da909de492f2e98fa0b8deee780443
RealMentalMan <144133729+RealMentalMan@users.noreply.github.com>
2024-03-19 12:27:34 +0300

    Update player.py
    И заправка, и пираты

--- player.py
+++ player.py
@@ -13,6 +13,7 @@ class Player:
         if now_distance <= self.fuel:
             self.fuel -= now_distance
             self.fuel += destination.fuel_station_value
+            self.fuel -= destination.pirates_value
             destination.fuel_station_value = 0
             self.current_planet = destination
             self.visited_planets.append(destination)
