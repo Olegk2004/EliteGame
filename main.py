f0416650f47429e7b142f2cc3a16cded491a45ce
RealMentalMan <144133729+RealMentalMan@users.noreply.github.com>
2024-03-19 11:20:48 +0300

    Update main.py
    Принтует пока что заправку, clear_output() у меня не работает

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 # Заготовка для игры
 import pygame, sys
 from settings import *
-
+from math import sqrt
 from IPython.display import clear_output
 from galaxy import Galaxy
 from player import Player
 from galaxy_plot import create_plot
 from galaxy_map import *
 import galaxy_map
-
-
+from IPython.display import clear_output
+def distance(a, b):
+  return int(4*sqrt((a.x-b.x)*(a.x-b.x)+(a.y-b.y)*(a.y-b.y)/4))
 class Game:
     def __init__(self):
         pygame.init()
@@ -24,13 +25,22 @@ class Game:
     def run(self):
         new_galaxy = Galaxy(0x5A4A, 0x0248, 0xB753)
         new_galaxy.make_systems()
+
         new_galaxy.create_matches()
         player = Player(new_galaxy)
         # create_plot(new_galaxy)  # Изображение графа связей планет
         pygame.mixer.music.play(-1)
         music_is_muted = False
+        print(f"Приветствуем вас, путник! Вы находитесь на планете:{player.current_planet.name}")
+        print(f"У вас топлива: {player.fuel}")
+        available = new_galaxy.matches[player.current_planet]
+        print("Можете прыгнуть до: ")
+        for i in range(len(available)):
+
+            print(f"{i + 1}.{available[i].name}, необходимо иметь топлива: {distance(available[i], player.current_planet)}")
         running = True
         while running:
+
             map = Map(new_galaxy, player)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -39,11 +49,25 @@ class Game:
                     sys.exit()
                 elif event.type == pygame.MOUSEBUTTONDOWN:
                     if event.button == 1:  # Если нажата левая кнопка мыши
+
                         click_pos = pygame.mouse.get_pos()
+
                         clicked_planet = map.check_click(click_pos)
                         if clicked_planet:
+                            clear_output()
                             player.jump(clicked_planet)
+                            available = new_galaxy.matches[player.current_planet]
+                            print("\n" * 10)
+                            print(f"Вы находитесь на планете:{player.current_planet.name}")
+                            if player.current_planet.flag != 0:
+                                print(f"Вау, здесь етсь заправка, вы заправились на {player.current_planet.fuel_station_value_save} топлива")
+                            print(f"У вас топлива: {player.fuel}")
+                            print("Можете прыгнуть до: ")
+                            for i in range(len(available)):
+                                print(
+                                    f"{i + 1}.{available[i].name}, необходимо иметь топлива: {distance(available[i], player.current_planet)}")
                             self.screen.fill((0, 0, 0))
+
                 elif event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_m:
                         if music_is_muted:
@@ -55,6 +79,7 @@ class Game:
 
             map.all_sprites.update()
             map.draw(self.screen)
+
             pygame.display.update()
 
 
