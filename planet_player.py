57f0e6afd9e93222227ed42a87e5a9100ab87b22
Olegk2004 <olegk_wot@mail.ru>
2024-03-23 10:30:14 +0300

    Добавил переключение между космосом и планетой

--- planet_player.py
+++ planet_player.py
@@ -6,6 +6,6 @@ class PlanetPlayer(pygame.sprite.Sprite):
     def __init__(self, pos, group):
         super().__init__(group)
 
-        self.image = pygame.Surface((64, 32))
+        self.image = pygame.Surface((8, 8))
         self.image.fill('green')
         self.rect = self.image.get_rect(center=pos)
