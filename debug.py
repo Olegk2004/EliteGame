68bdf02ad4daa0861592a1318f28f617a6dcf1c5
berberberk <pedchenkoti@gmail.com>
2024-03-26 00:50:31 +0300

    Добавил аргумент display_surf для указания поверхности вывода сообщения

--- debug.py
+++ debug.py
@@ -7,8 +7,7 @@ pygame.init()
 font = pygame.font.Font(None, 30)
 
 
-def debug(message, y=10, x=10):
-    display_surf = pygame.display.get_surface()
+def debug(message, display_surf=pygame.display.get_surface(), y=10, x=10):
     debug_surf = font.render(str(message), True, 'yellow')
     debug_rect = debug_surf.get_rect(topleft=(x, y))
     pygame.draw.rect(display_surf, 'black', debug_rect)
