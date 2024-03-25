b6e30a5926e8d76b9b0f714e5c672555bc245109
berberberk <pedchenkoti@gmail.com>
2024-03-26 01:03:34 +0300

    Добавил несколько констант: MAP_PANEL_WIDTH -- Ширина панели, на которой отображается карта MAP_PANEL_HEIGHT -- Высота панели, на которой отображается карта FUEL_CONST = 2500 -- Константа, связанная с топливом. С помощью нее рассчитывается стартовый запас топлива и параметры шкалы топлива на боковой панели. MAP_WIDTH и MAP_HEIGHT теперь отвечают за параметры поверхности самой карты, фрагмент которой отображается на панели MAP_PANEL

--- settings.py
+++ settings.py
@@ -2,8 +2,10 @@ import pygame
 
 SCREEN_WIDTH = 1050
 SCREEN_HEIGHT = 750
-MAP_WIDTH = 750
-MAP_HEIGHT = 750
+MAP_WIDTH = 3000
+MAP_HEIGHT = 3000
+MAP_PANEL_WIDTH = 750
+MAP_PANEL_HEIGHT = 750
 EDGES_COLOR = [255, 255, 0]
 CURRENT_PLANET_IMAGE = pygame.image.load("Images/Current_Planet.png")
 STANDART_PLANET_IMAGE = pygame.image.load("Images/Standard_Planet.png")
@@ -11,4 +13,5 @@ CAPITAL_PLANET_IMAGE = pygame.image.load("Images/Capital_Planet.png")
 FUEL_STATION_PLANET_IMAGE = pygame.image.load("Images/Fuel_Planet.png")
 SUPER_FUEL_PLANET_IMAGE = pygame.image.load("Images/Super_Fuel.png")
 # PLAYER_IMAGE = pygame.image.load("Images/7rb7.gif")
-MAX_FUEL_VALUE = 1480
+FUEL_CONST = 2500
+MAX_FUEL_VALUE = int(FUEL_CONST * 1.48)
