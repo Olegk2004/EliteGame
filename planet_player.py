82a0a4175ef8059104ecb2a57ad831e946a5dc34
berberberk <pedchenkoti@gmail.com>
2024-03-31 01:55:16 +0300

    Добавил выбор и использование инструмента

--- planet_player.py
+++ planet_player.py
@@ -1,6 +1,6 @@
 import pygame
 from settings import *
-
+from timer import Timer
 
 class PlanetPlayer(pygame.sprite.Sprite):
     def __init__(self, pos, group):
@@ -8,7 +8,7 @@ class PlanetPlayer(pygame.sprite.Sprite):
 
         self.image_status = "idle"  # для определения какого направления спрайт вставлять
         self.image_frame = 1  # для реализации анимации
-        # для анимации нужны несколько картинок в одном направлении, чтобы они менялись каждые несколько милисекунд
+        # для анимации нужны несколько картиок в одном направлении, чтобы они менялись каждые несколько милисекунд
 
         self.image = self.import_image()
         self.rect = self.image.get_rect(center=pos)
@@ -17,6 +17,17 @@ class PlanetPlayer(pygame.sprite.Sprite):
         self.direction = pygame.math.Vector2()  # направление, определяется вектором. Во время обновления координаты игрока меняются в зависимости от направления
         self.pos = pygame.math.Vector2(self.rect.center)  # координаты игрока
         self.speed = 200
+
+        # Таймеры
+        self.timers = {
+            'tool use': Timer(350, self.use_tool),
+            'tool switch': Timer(200)
+        }
+
+        # Инструменты
+        self.tools = ['hand', 'gun']
+        self.tool_index = 0
+        self.selected_tool = self.tools[self.tool_index]
 
     def import_image(self):
         path = "Images/player_" + self.image_status + "_" + str(int(self.image_frame) + 1) + ".png"
@@ -33,26 +44,51 @@ class PlanetPlayer(pygame.sprite.Sprite):
     def input(self):
         keys = pygame.key.get_pressed()
 
-        if keys[pygame.K_UP]:
-            self.direction.y = -1
-            self.image_status = "up"
-        elif keys[pygame.K_DOWN]:
-            self.direction.y = 1
-            self.image_status = "down"
-        else:
-            self.direction.y = 0
+        # Если игрок в процессе использования инструмента,
+        # он не может двигаться и сменить инструмент
+        if not self.timers['tool use'].active:
+            # Передвижение на WASD
+            if keys[pygame.K_w]:
+                self.direction.y = -1
+                self.image_status = "up"
+            elif keys[pygame.K_s]:
+                self.direction.y = 1
+                self.image_status = "down"
+            else:
+                self.direction.y = 0
 
-        if keys[pygame.K_LEFT]:
-            self.direction.x = -1
-            self.image_status = "left"
-        elif keys[pygame.K_RIGHT]:
-            self.direction.x = 1
-            self.image_status = "right"
-        else:
-            self.direction.x = 0
+            if keys[pygame.K_a]:
+                self.direction.x = -1
+                self.image_status = "left"
+            elif keys[pygame.K_d]:
+                self.direction.x = 1
+                self.image_status = "right"
+            else:
+                self.direction.x = 0
+
+            # Использование инструмента
+            if keys[pygame.K_f]:
+                self.timers['tool use'].activate()
+                self.direction = pygame.math.Vector2()
+                self.image_frame = 0
+
+            # Смена инструмента
+            if keys[pygame.K_TAB] and not self.timers['tool switch'].active:
+                self.timers['tool switch'].activate()
+                self.tool_index += 1
+                if self.tool_index >= len(self.tools):
+                    self.tool_index = 0
+                self.selected_tool = self.tools[self.tool_index]
 
         if self.direction.x == 0 and self.direction.y == 0:  # если остаемся на месте
             self.image_status = "idle"
+
+    def update_timers(self):
+        for timer in self.timers.values():
+            timer.update()
+
+    def use_tool(self):
+        return
 
     def move(self, dt):
         # Нормализация ветора. Это нужно, чтобы скорость по диагонали была такая же
@@ -71,5 +107,7 @@ class PlanetPlayer(pygame.sprite.Sprite):
 
     def update(self, dt):
         self.input()
+        self.update_timers()
+
         self.move(dt)
         self.animate(dt)
