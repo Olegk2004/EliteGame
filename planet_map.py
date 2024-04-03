4b2007824936347b13a190d38afd974195cec48d
RealMentalMan <144133729+RealMentalMan@users.noreply.github.com>
2024-04-03 21:48:43 +0300

    Добавил тайл-карту и осязаемые объекты
    Добавил тайл-карту и осязаемые объекты

--- planet_map.py
+++ planet_map.py
@@ -7,6 +7,24 @@ from debug import debug
 from planet_player import PlanetPlayer
 from planet_enemy import PlanetEnemy
 from overlay import Overlay
+from pytmx.util_pygame import load_pygame
+
+# pip install pytmx
+
+class Collisions(pygame.sprite.Sprite): #пока не использую, мало ли пригодиться( класс осязаемых объектов)
+    def __init__(self, pos, image):
+        super().__init__()
+        self.image = image
+        self.rect = self.image.get_rect(center=pos)
+
+        # movement attributes
+        self.direction = pygame.math.Vector2()  # направление, определяется вектором. Во время обновления координаты игрока меняются в зависимости от направления
+        self.pos = pygame.math.Vector2(self.rect.center)
+class Tile(pygame.sprite.Sprite):# пока не использую, класс тайл-карт
+    def __init__(self, pos, surf):
+        super().__init__()
+        self.image = surf
+        self.rect = self.image.get_rect(topleft=pos)
 
 
 class PlanetMap:
@@ -15,26 +33,52 @@ class PlanetMap:
         self.all_sprites = pygame.sprite.Group()
         self.bullet_group = pygame.sprite.Group()
         self.display_surface = pygame.display.get_surface()
-        self.setup()
+        self.stat = 0 # флаг на одноразовый вызов метоа сэтап (там мы передаём позиции в планет_мап)
+        self.stat2 = 1# флаг на одноразовое заполнение массива позиций
 
-    def setup(self):
-        self.planet_player = PlanetPlayer((100, 100), self.all_sprites)
+        self.coll = [] # массив позиций
+        #self.setup()
+
+    def setup(self, coll_pos):
+        self.planet_player = PlanetPlayer((50, 350), self.all_sprites, coll_pos) # теперь в аргументах указываем ещё и позиции осязаемых объектов
+
         self.planet_enemies = {}
         self.enemies_overlays = {}
-        for i in range(3):
-            self.planet_enemies['enemy_' + str(i)] = PlanetEnemy(self.planet_player, self.bullet_group,
-                                                                 self.all_sprites, (500, 100 + i * 100))
-            self.enemies_overlays['enemy_' + str(i)] = Overlay(self.display_surface,
-                                                               self.planet_enemies['enemy_' + str(i)])
-        self.overlay = Overlay(self.display_surface, self.planet_player)
+        #for i in range(NUM_OF_ENEMIES): # в сеттингс добавил кол-во врагов, пока закомментил
+            #self.planet_enemies['enemy_' + str(i)] = PlanetEnemy(self.planet_player, self.bullet_group,
+                                                                 #self.all_sprites, (500, 100 + i * 100))
+            #self.enemies_overlays['enemy_' + str(i)] = Overlay(self.display_surface,
+                                                               #self.planet_enemies['enemy_' + str(i)])
+        
 
+        self.overlay = Overlay(self.display_surface, self.planet_player)
     def draw(self, dt):
         self.display_surface.fill('black')
+        tmx_data = load_pygame("Tiles/tile2.tmx") # берём тайл-карту
+
+        for layer in tmx_data.visible_layers: # по всем видимым уровням тайл-карты
+
+            if hasattr(layer, 'data'): # необязательно
+                for x, y, surf in layer.tiles():# каждый икс и игрек и поверхность(рисуночек тайла отдельного) текущего уровня
+                    pos = (x * 32, y * 32) #tile выравниваем каждый тайл
+                    tile = Tile(pos = pos, surf = surf) # пока не надо
+                    if layer.name == "second" and self.stat2 != 0:# если это тайл второго уровня(гле осязаемые объекты) и при этом мы добавляли его позиции ниразу, то
+
+                        self.coll.append([x * 32, y * 32]) # добавляем позиции осязаемого объекта
+                    self.display_surface.blit(surf, pos)# блитуем
+
+
         for sprite in sorted(self.all_sprites, key=lambda sprite: sprite.rect.centery):
             self.display_surface.blit(sprite.image, sprite.rect.topleft)
+
         self.bullet_group.draw(self.display_surface)
         self.all_sprites.update(dt)
         self.bullet_group.update(dt)
-        for overlay in self.enemies_overlays.values():
-            overlay.display()
+        #for overlay in self.enemies_overlays.values():
+            #overlay.display()
+        if self.stat == 0: # меняем необходимые флаги
+            self.setup(self.coll) # передаём позиции осязаемых позиций
+            self.stat = 1
+            self.stat2 = 0
+
         self.overlay.display()
