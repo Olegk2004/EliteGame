2a1cdda4b5bae9abaf200e7bf65f2961be92db35
berberberk <pedchenkoti@gmail.com>
2024-04-04 23:49:04 +0300

    Добавил отображение ХП и выбранного оружия. Но пока что это просто визуал.

--- overlay.py
+++ overlay.py
@@ -1,16 +1,33 @@
 import pygame
 from settings import *
 
+
 class Overlay:
     def __init__(self, display_surface, planet_player):
         self.display_surface = display_surface
         self.planet_player = planet_player
         # overlay_path = ''
-        self.tools_surf = [pygame.surface.Surface((10, 10)), pygame.surface.Surface((10, 10))]
-        self.tools_surf[0].fill('green') # Выбрана рука
-        self.tools_surf[1].fill('pink') # Выбрана пушка
+        self.tools_surf = self.planet_player.tools_sprites
 
     def display(self):
-        tool_surf = self.tools_surf[self.planet_player.tool_index]
-        #tool_rect = tool_surf.get_rect(topleft=(self.planet_player.rect.topleft[0] - 5, self.planet_player.rect.topleft[1] - 5))
-        #self.display_surface.blit(tool_surf, tool_rect)
+        tool_surf = self.tools_surf[self.planet_player.selected_tool]
+        tool_rect = tool_surf.get_rect(topleft=(10, 10))
+        self.display_surface.blit(tool_surf, tool_rect)
+
+        hp_bar_bgd = pygame.Surface((500, 20))
+        hp_bar_bgd.fill('black')
+        hp_bar = pygame.Surface((self.planet_player.hp / self.planet_player.max_hp * 500, 20))
+        hp_bar.fill('red')
+        hp_bar_bgd_rect = hp_bar_bgd.get_rect(topleft=(70, 10))
+        hp_bar_rect = hp_bar.get_rect(topleft=(70, 10))
+        self.display_surface.blit(hp_bar_bgd, hp_bar_bgd_rect)
+        self.display_surface.blit(hp_bar, hp_bar_rect)
+
+        stamina_bar_bgd = pygame.Surface((250, 20))
+        stamina_bar_bgd.fill('black')
+        stamina_bar = pygame.Surface((self.planet_player.stamina / self.planet_player.max_stamina * 250, 20))
+        stamina_bar.fill('blue')
+        stamina_bar_bgd_rect = stamina_bar_bgd.get_rect(topleft=(70, 40))
+        stamina_bar_rect = stamina_bar.get_rect(topleft=(70, 40))
+        self.display_surface.blit(stamina_bar_bgd, stamina_bar_bgd_rect)
+        self.display_surface.blit(stamina_bar, stamina_bar_rect)
