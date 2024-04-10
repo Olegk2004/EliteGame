3693fd7edbfb1ac6f1a07a614cd40fea393993ea
Olegk2004 <olegk_wot@mail.ru>
2024-04-10 20:14:11 +0300

    Добавил анимацию персонажу и коллизию

---
+++ Tiles/Hills.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Hills" tilewidth="32" tileheight="32" tilecount="15" columns="5">
+ <image source="Sprout Lands - Sprites - Basic pack/Tilesets/Hills.png" width="176" height="112"/>
+</tileset>
